af5ea773c23303ad57e329ecda736f9de1992292
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-11-25 20:07:29 +0100

    players can join the game

--- src/clj/multi_client_ws/routes/websockets.clj
+++ src/clj/multi_client_ws/routes/websockets.clj
@@ -1,7 +1,8 @@
 (ns multi-client-ws.routes.websockets
   (:require [compojure.core :refer [GET defroutes wrap-routes]]
             [clojure.tools.logging :as log]
-            [immutant.web.async :as async]))
+            [immutant.web.async :as async]
+            [cognitect.transit :as transit]))
 
 (defonce channels (atom #{}))
 
@@ -13,15 +14,35 @@
   (log/info "close code:" code "reason:" reason)
   (swap! channels #(remove #{channel} %)))
 
+(defn- decode [msg]
+  (-> msg
+      (.getBytes "UTF-8")
+      (java.io.ByteArrayInputStream.)
+      (transit/reader :json)
+      (transit/read)))
+
+(defn- encode [msg]
+  (let [out (java.io.ByteArrayOutputStream. 4096)
+        writer (transit/writer out :json)]
+    (transit/write writer msg)
+    (.toString out)))
+
 (defn notify-clients! [channel msg]
-  (doseq [channel @channels]
-    (async/send! channel msg)))
+  (let [message (encode msg)]
+    (log/info "notify clients:" msg)
+    (doseq [channel @channels]
+      (async/send! channel message))))
+
+(defn dispatch-message! [channel msg]
+  (let [message (:message (decode msg))]
+    (case (:type message)
+      :join (notify-clients! channel {:message (str (:name message) " joined the game")}))))
 
 (def websocket-callbacks
   "WebSocket callback functions"
   {:on-open    connect!
    :on-close   disconnect!
-   :on-message notify-clients!})
+   :on-message dispatch-message!})
 
 (defn ws-handler [request]
   (async/as-channel request websocket-callbacks))
--- src/cljs/multi_client_ws/core.cljs
+++ src/cljs/multi_client_ws/core.cljs
@@ -10,33 +10,47 @@
      ^{:key i}
      [:li message])])
 
-(defn message-input []
-  (let [value (atom nil)]
-    (fn []
-      [:input.form-control
-       {:type        :text
-        :placeholder "type in a message and press enter"
-        :value       @value
-        :on-change   #(reset! value (-> % .-target .-value))
-        :on-key-down
-                     #(when (= (.-keyCode %) 13)
-                        (ws/send-transit-msg!
-                          {:message @value})
-                        (reset! value nil))}])))
+(defn input-element
+  "An input element which updates its value on change"
+  [label id name type value]
+  [:div
+   [:label label]
+   [:input {:id          id
+            :name        name
+            :class       "form-control"
+            :type        type
+            :required    ""
+            :value       @value
+            :on-change   #(reset! value (-> % .-target .-value))
+            :on-key-down #(when (= (.-keyCode %) 13)
+                            (ws/send-transit-msg!
+                              {:message {:type :join :name @value} })
+                            (reset! value nil))}]])
+
+(defn player-name-element
+  [player-name-atom]
+  (input-element
+    "What's your name?"
+    "player-name"
+    "player-name"
+    "text"
+    player-name-atom))
 
 (defn home-page []
-  [:div.container
-   [:div.row
-    [:div.col-md-12
-     [:h2 "Let's play Pigs"]]]
-   [:div.row
-    [:div.col-sm-6
-     [message-list]]]
-   [:div.row
-    [:div.col-sm-6
-     [message-input]]]])
+  (let [player-name (atom nil)]
+    [:div.container
+     [:div.row
+      [:div.col-md-12
+       [:h2 "Let's play Pigs"]]]
+     [:div.row
+      [:div.col-sm-6
+       [message-list]]]
+     [:div.row
+      [:div.col-sm-6
+       [player-name-element player-name]]]]))
 
 (defn update-messages! [{:keys [message]}]
+  (println "Received message:" message)
   (swap! messages #(vec (take 10 (conj % message)))))
 
 (defn mount-components []
